8ee6f896729d28e400ebce6dbd426ac80e2696ee
NoahR02 <noahreppert95@gmail.com>
2020-04-20 03:06:56 -0400

    netlify deploy

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
-const isProd = (process.env.NODE_ENV || "production") === "production";
+
 module.exports = {
   exportPathMap: function() {
     return {
       "/": { page: "/" }
     }
-  },
-  assetPrefix: isProd ? "https://noahr02.github.io/portfolio" : "",
+  }
 }
